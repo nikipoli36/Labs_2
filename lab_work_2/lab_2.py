c6487c875744535ad55082f6ffce5e4d6895a67b
nikipoli36 <chukhrov.nikita@mail.ru>
2024-10-09 23:11:46 +0300

    Add files via upload

---
+++ lab_work_2/lab_2.py
@@ -0,0 +1,10 @@
+import random
+
+random.seed(22)
+a = random.randint(1, 10)
+b = random. randint(1, 13)
+c = random. randint(1, 13)
+
+print("Номера заданий для уровней 1, 2 и 3 соответсвенно >>")
+print(a , b , c, sep = ', ')
+
